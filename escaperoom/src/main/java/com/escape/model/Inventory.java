c5d390d1a44c03ee05327ace54f8aefe660791f2
talankinard <kinardtd@email.sc.edu>
2025-10-03 11:44:26 -0400

    Method stubs for inventory #30

---
+++ escaperoom/src/main/java/com/escape/model/Inventory.java
@@ -0,0 +1,63 @@
+package com.escape.model;
+
+import java.util.ArrayList;
+
+/**
+ * Class for all functions inventory related.
+ * Stores items and has actions such as add, remove, clear, and view.
+ * 
+ * @author Talan Kinard
+ */
+
+public class Inventory 
+{
+    public ArrayList<Inventory> inventory;
+    public String items;
+    public int capacity;
+
+
+    /**
+     * Returns the list of inventory items
+     * @return the inventory list
+     */
+    public ArrayList<Inventory> getInventory()
+    {
+        return null;
+    }
+
+    /**
+     * Adds an item to the inventory.
+     */
+    public void addInventory()
+    {
+        
+    }
+
+    /**
+     * Clears the inventory of all items.
+     */
+    public void clearInventory()
+    {
+        
+    }
+
+
+    /**
+     * Removes an object from inventory by name.
+     * 
+     * @param item, the item to remove
+     */
+    public void removeFromInventory(String item)
+    {
+        
+    }
+ 
+    /**
+     * Displays the current inventory
+     */
+    public void viewInventory()
+    {
+        
+    }
+
+}
