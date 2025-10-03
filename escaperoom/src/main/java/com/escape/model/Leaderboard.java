76d204a8fa1c508a0e4e57960ccc84b0dc23e868
talankinard <kinardtd@email.sc.edu>
2025-10-03 12:31:45 -0400

    Finished leaderboard method stubs #32

---
+++ escaperoom/src/main/java/com/escape/model/Leaderboard.java
@@ -0,0 +1,54 @@
+package com.escape.model;
+
+import java.util.ArrayList;
+
+/**
+ * Class for representation of the game leaderboard.
+ * Stores user entries and contains methods for retrieving.
+ * 
+ * @author Talan Kinard
+ */
+
+public class Leaderboard 
+{
+    /**
+     * List of users currently on the leaderboard.
+     */
+    public ArrayList<User> entries;
+
+    /**
+     * 
+     * @param d the difficulty level.
+     * @return a list of scores in relation with the difficulty level.
+     */
+    public ArrayList<Score> topByDifficulty(Difficulty d)
+    {
+        return null;
+    }
+
+    /**
+     * Gets leaderboard entries
+     * @return current leaderboard entries
+     */
+    public ArrayList<User> getLB()
+    {
+        return null;
+    }
+
+    /**
+     * Sets the leaderboard entries
+     * @param entries a list of users to the leaderboard.
+     */
+    public void setLB(ArrayList<User> entries)
+    {
+
+    }
+
+    /**
+     * Prints the leaderboard.
+     */
+    public void printLB()
+    {
+        System.out.println("Printing Leaderboard..."); //hardcode stub for now
+    }
+}
