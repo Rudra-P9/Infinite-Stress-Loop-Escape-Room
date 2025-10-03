bb80b979b91a08634348adad7876c867168a4ae5
talankinard <kinardtd@email.sc.edu>
2025-10-03 12:01:55 -0400

    EscapeRoomFacade class stubs #24

---
+++ escaperoom/src/main/java/com/escape/model/EscapeRoomFacade.java
@@ -0,0 +1,96 @@
+package com.escape.model;
+
+/**
+ * Facade class to organize the Escape Room.
+ * Interface that provides game functions to start, end, pause, resuming, save, and loading.
+ * 
+ * @author Talan Kinard
+ */
+
+public class EscapeRoomFacade 
+{
+    /**
+     * Should be noted won't work until the implementation of these classes.
+     * Surrounded by comments for now.
+     */
+
+    /** 
+    private User currentUser;
+    private Settings settings;
+    private Room currentRoom;
+    private Timer timer;
+    private SavedData savedData;
+    */
+
+    /**
+     * Starts a game session.
+     */
+    public void startGame()
+    {
+
+    }
+
+    /**
+     * Ends a game session.
+     */
+    public void endGame()
+    {
+
+    }
+
+    /**
+     * Pauses a game session.
+     */
+    public void pauseGame()
+    {
+
+    }
+
+    /**
+     * Resumes a game session.
+     */
+    public void resumeGame()
+    {
+        
+    }
+
+    /**
+     * Saves the current game.
+     */
+    public void saveGame()
+    {
+
+    }
+
+    /**
+     * Loads a previously saved game.
+     */
+    public void loadGame()
+    {
+
+    }
+
+    /**
+     * Displays a hint to the player.
+     */
+    public void getHint()
+    {
+    
+    }
+
+    /**
+     * Retrieves info about the user
+     */
+    public void getUser()
+    {
+
+    }
+
+    /**
+     * Gets the current room.
+     */
+    public void getRoom()
+    {
+
+    }
+}
