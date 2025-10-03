983ca6abe538f451566a2f3e4b1ee5f6289db564
talankinard <kinardtd@email.sc.edu>
2025-10-03 12:48:31 -0400

    Difficulty enum stubs finished #34

---
+++ escaperoom/src/main/java/com/escape/model/Difficulty.java
@@ -0,0 +1,14 @@
+package com.escape.model;
+
+/**
+ * Enumeration for the difficulty levels of this Escape Room.
+ * Difficulty is used to control the time that the player will have to complete the Escape Room.
+ * 
+ * @author Talan Kinard
+ */
+public enum Difficulty 
+{
+    EASY,
+    MEDIUM,
+    HARD
+}
