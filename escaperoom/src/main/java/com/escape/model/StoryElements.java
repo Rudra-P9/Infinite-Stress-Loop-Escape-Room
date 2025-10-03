52193ba5839277f3016f309b8f8ce91d1811f091
talankinard <kinardtd@email.sc.edu>
2025-10-03 13:03:03 -0400

    Finsihed story elements method stubs #39

---
+++ escaperoom/src/main/java/com/escape/model/StoryElements.java
@@ -0,0 +1,32 @@
+package com.escape.model;
+
+import java.util.ArrayList;
+
+/**
+ * Class for the story elements of the Escape Room.
+ * Methods to get the story text and convert to String.
+ */
+public class StoryElements 
+{
+    private ArrayList<Story> story;
+    private String storyText;
+    private static final int storyPos = 0;
+
+
+    /**
+     * @return story text
+     */
+    public String getStory()
+    {
+        return null;
+    }
+
+    /**
+     * String representation of story elements
+     */
+    @Override
+    public String toString()
+    {
+        return "";
+    }
+}
