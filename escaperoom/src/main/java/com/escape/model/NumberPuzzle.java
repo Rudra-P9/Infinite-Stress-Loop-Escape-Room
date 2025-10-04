517c533e1f10e5c6d972a4ab7059eeaa1c31d246
KinardJacob <jacobkinard1@gmail.com>
2025-10-04 16:10:24 -0400

    added NumberPuzzle class method stubs #29

--- escaperoom/src/main/java/com/escape/model/NumberPuzzle.java
+++ escaperoom/src/main/java/com/escape/model/NumberPuzzle.java
@@ -2,18 +2,18 @@ package com.escape.model;
 import java.util.UUID;
 
 /**
- * Child class responsible for managing any and all "word" puzzles.
+ * Child class responsible for managing any and all "number" puzzles.
  * @author Jacob Kinard
  * 
  */
-public class WordPuzzle extends Puzzle{
+public class NumberPuzzle extends Puzzle{
     /**
      *  
      *
      * @param title - The title of the puzzle
      * @param objective - Is the objective of the puzzle and is a description of what the user is trying to solve.
      * @param solved - holds the vlaue if the puzzle is solved or not
-     * @param solution - holds the valid solution to the puzzle
+     * @param solution - holds the valid number solution to the puzzle
      * @param puzzleID - holds a uniqe identifier address for puzzle object
      * 
      */
@@ -21,9 +21,9 @@ public class WordPuzzle extends Puzzle{
     private String title;
     private String objective;
     private boolean solved;
-    private String solution;
+    private int solution;
 
-    public WordPuzzle(String title, String objective, String solution, boolean solved){
+    public NumberPuzzle(String title, String objective, int solution, boolean solved){
         this.title = title;
         this.objective = objective;
         this.puzzleID = puzzleID;
