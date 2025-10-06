90ca714696ba99c1b75a978b351ed0ae74358d63
talankinard <kinardtd@email.sc.edu>
2025-10-06 18:47:11 -0400

    fixed typo in StoryElements #39

--- escaperoom/src/main/java/com/escape/model/StoryElements.java
+++ escaperoom/src/main/java/com/escape/model/StoryElements.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
  */
 public class StoryElements 
 {
-    private ArrayList<Story> story;
+    private ArrayList<StoryElements> story;
     private String storyText;
     private static final int storyPos = 0;
 
