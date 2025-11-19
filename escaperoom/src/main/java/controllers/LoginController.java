2cf75cde679155ca06c61134f39a0a26344201f4
KinardJacob <jacobkinard1@gmail.com>
2025-11-19 01:31:49 -0500

    added Login GUI example, removed test fxml and controller #122

---
+++ escaperoom/src/main/java/controllers/LoginController.java
@@ -0,0 +1,30 @@
+package controllers;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.input.MouseEvent;
+
+import com.escape.App;
+
+/**
+ *
+ * @author Jacob Kinard
+ */
+
+public class LoginController implements Initializable {
+
+    @FXML
+    private void goBack(MouseEvent event) throws IOException {
+        App.setRoot("Landing");
+    }
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+    }
+
+}
