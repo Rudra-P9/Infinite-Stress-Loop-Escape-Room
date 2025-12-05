6077f744e23010baabeba6cf4406b927f601ff98
dbdiaz <dbdiaz0326@gmail.com>
2025-12-04 19:05:03 -0500

    Got hint button in final puzzle working #160

--- escaperoom/src/main/java/controllers/FinalPuzzleController.java
+++ escaperoom/src/main/java/controllers/FinalPuzzleController.java
@@ -1,36 +1,53 @@
 package controllers;
 
+import javafx.animation.FadeTransition;
 import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.image.ImageView;
-import javafx.scene.control.Label;
+import javafx.scene.text.TextFlow;
+import javafx.util.Duration;
 
 public class FinalPuzzleController {
 
-    @FXML private ImageView hintIcon;    
-    @FXML private AnchorPane hintPane;   
-    @FXML private Label hintText;        
-    @FXML private ImageView closeHint;   
+    @FXML private ImageView FPHintIcon;
+    @FXML private AnchorPane hintPane;
+    @FXML private TextFlow hintBottomFlow;
 
     @FXML
     private void initialize() {
-        if (hintPane != null) {
-            hintPane.setVisible(false);
+        javafx.scene.text.Font.loadFont(getClass().getResourceAsStream("/fonts/Storm Gust.ttf"), 36);
+        if (hintPane != null) hintPane.setVisible(false);
+        if (hintBottomFlow != null) {
+            hintBottomFlow.setVisible(false);
+            hintBottomFlow.setOpacity(0.0);
         }
+        if (FPHintIcon != null) FPHintIcon.setFocusTraversable(false);
     }
 
     @FXML
     private void showHint(MouseEvent event) {
-        if (hintPane != null) {
-            hintPane.setVisible(true);
+        if (FPHintIcon != null) FPHintIcon.setVisible(false);
+        if (hintPane != null) hintPane.setVisible(false);
+        if (hintBottomFlow != null) {
+            hintBottomFlow.setOpacity(0.0);
+            hintBottomFlow.setVisible(true);
+            FadeTransition ft = new FadeTransition(Duration.millis(450), hintBottomFlow);
+            ft.setFromValue(0.0);
+            ft.setToValue(1.0);
+            ft.play();
         }
     }
 
     @FXML
     private void hideHint(MouseEvent event) {
-        if (hintPane != null) {
-            hintPane.setVisible(false);
+        if (hintBottomFlow != null && hintBottomFlow.isVisible()) {
+            FadeTransition ft = new FadeTransition(Duration.millis(300), hintBottomFlow);
+            ft.setFromValue(hintBottomFlow.getOpacity());
+            ft.setToValue(0.0);
+            ft.setOnFinished(e -> hintBottomFlow.setVisible(false));
+            ft.play();
         }
+        if (FPHintIcon != null) FPHintIcon.setVisible(true);
     }
 }
