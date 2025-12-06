ab57e9db22ab4bbc68b1511956ecfc98158a3cba
talankinard <kinardtd@email.sc.edu>
2025-12-05 21:44:23 -0500

    fixed app

--- escaperoom/src/main/java/com/escape/App.java
+++ escaperoom/src/main/java/com/escape/App.java
@@ -42,7 +42,7 @@ public class App extends Application {
         }
 
         // Load the initial view wrapped in the scaler
-        Parent root = loadFXML("FragmentCorridor");
+        Parent root = loadFXML("MainScreen");
 
         // Create scene with the scalable root
         scene = new Scene(root, 640, 480);
